06d110307b21a8ab567927202700a6791721ede7
dwclake <devonwebb559@gmail.com>
2023-04-17 07:02:50 -0700

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,21 @@ int main() {
     using namespace menu;
 
     bool exit = false;
+    auto fn = [&](Menu* menu){
+        std::system("clear");
+
+        printf("%s", menu->title().c_str());
+
+        menu->for_each([](Menu sub){
+                std::printf("%s", sub.title().c_str());
+                });
+
+        exit = true;
+    };
 
     Menu main_menu = MenuBuilder::create()
         ->title(" -- Main Menu --\n")
-        ->fn([&](Menu* menu){
-            std::system("clear");
-
-            printf("%s", menu->title().c_str());
-
-            menu->for_each([](Menu sub){
-                std::printf("%s", sub.title().c_str());
-            });
-
-            exit = true;
-        })
+        ->fn(fn)
         ->build();
 
     while(!exit) main_menu();    
