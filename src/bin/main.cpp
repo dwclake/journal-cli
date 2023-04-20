b5a56ffa8e2dd47cf3412b519700400185fb736e
dwclake <devonwebb559@gmail.com>
2023-04-20 13:48:26 -0700

    Adjusted main and it's documentation

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -39,8 +39,16 @@ int main(int argc, char* argv[]) {
     Journal journal; // Create new journal object
     // Create a page with a title and a body
     Page page = Page::builder()
-        ->title("Hello world")
-        ->body("Hows it going yall.\nIdk whats going on.\n")
+        ->title("Dobar dan!")
+        ->body("    Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit\
+enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.\
+Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem\
+est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia\
+pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit\
+commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident\
+adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua\
+reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris\
+sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.")
         ->build();
     // Insert page into the journal
     journal.insert(page);
@@ -54,6 +62,9 @@ int main(int argc, char* argv[]) {
         menu->for_each([](Menu sub){
                 std::printf("%s", sub.title().c_str());
         });
+        
+        printf("Journal entries:\n");
+        journal.display(); // Displays all pages in the journal
         // Set exit to true to end main loop
         exit = true;
     };
@@ -63,13 +74,11 @@ int main(int argc, char* argv[]) {
     // Create new menu, main_menu, and give it a title and the display lambda with the key "main".
     // "main" function is automatically called when object is called as a function ie: main_menu();
     Menu main_menu = Menu::builder()
-        ->title(" -- Main Menu --\n")
+        ->title("\t\t\t -- Main Menu --\n")
         ->fn("main", main_display)
         ->build(); 
     
     while(!exit) main_menu(); // Run main menu until exit is set true    
-
-    journal.display(); // Display all the pages in journal
 
     return 0;
 }
