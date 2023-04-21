4cf8e74899da285061366b9b396d6040d5416edf
dwclake <devonwebb559@gmail.com>
2023-04-20 18:40:54 -0700

    Adjusted project structure

---
+++ test/test.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <tuple>
+#include "../includes/menu/menu.h"
+#include "../includes/journal/journal.h"
+
+namespace Test {
+    using namespace std;
+
+    vector<tuple<int, string>>* test();
+    tuple<int, string> test_journal_insert();
+    tuple<int, string> test_journal_fetch();
+    tuple<int, string> test_journal_remove();
+    tuple<int, string> test_journal_sort();
+}
